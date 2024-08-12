6863c1e8d711a81fc60b6f9b62237c0acf24caee
horrifyingHorse <excuseplanation@gmail.com>
2024-08-12 21:00:28 +0530

    user args separation

--- autoHead.cpp
+++ autoHead.cpp
@@ -1,9 +1,187 @@
+#include <cstddef>
+#include <cstdlib>
 #include <iostream>
 using namespace std;
 
+class CharQueue {
+private:
+  struct qNode {
+    struct qNode *plink;
+    string chr;
+    struct qNode *nlink;
+  };
+
+  struct qNode *head;
+  struct qNode *tail;
+
+public:
+  CharQueue() {
+    this->head = NULL;
+    this->tail = NULL;
+  }
+
+  string enq(string bffr) {
+    struct qNode *temp = new qNode;
+    temp->plink = NULL;
+    temp->chr = bffr;
+    temp->nlink = this->head;
+
+    if (this->head != NULL) {
+      this->head->plink = temp;
+    }
+
+    this->head = temp;
+
+    if (this->tail == NULL) {
+      this->tail = temp;
+    }
+
+    return bffr;
+  }
+
+  string deq() {
+    string bffr;
+
+    if (this->tail == NULL) {
+      return "";
+    }
+
+    bffr = this->tail->chr;
+
+    if (this->tail == this->head) {
+      this->head = NULL;
+      free(this->tail);
+      this->tail = NULL;
+      return bffr;
+    }
+
+    this->tail = this->tail->plink;
+    free(this->tail->nlink);
+    this->tail->nlink = this->head;
+
+    return bffr;
+  }
+
+  bool isEmpty() { return (this->tail == NULL) ? true : false; }
+};
+
+class ArgsList {
+private:
+  struct argsNode {
+    string arg;
+    struct argsNode *link;
+  };
+
+  struct argsNode *argTail;
+
+public:
+  ArgsList() { argTail = NULL; }
+
+  struct argsNode *argsNodeInsert(string bffr) {
+    struct argsNode *temp = new argsNode;
+    temp->arg = bffr;
+    temp->link = NULL;
+
+    // debug bffr
+    // cout << "temp: " << temp << "\t\t" << bffr << endl;
+
+    if (this->argTail == NULL) {
+      this->argTail = temp;
+      this->argTail->link = this->argTail;
+      return this->argTail;
+    }
+
+    temp->link = this->argTail->link;
+    this->argTail->link = temp;
+    this->argTail = temp;
+
+    return this->argTail;
+  }
+
+  void argsNodeDisplay() {
+    struct argsNode *temp = this->argTail->link;
+    bool flag = false;
+
+    while (!flag) {
+      cout << temp->arg << " :: ";
+
+      if (temp == this->argTail) {
+        flag = true;
+      }
+
+      temp = temp->link;
+    }
+
+    cout << endl;
+  }
+};
+
 class UsrCmd {
 private:
+  string usrInp;
   int totalArgs;
+  ArgsList usrArgs; // user args in a circular linked list
+
+  ArgsList *usrInpParser() {
+    //    int separator = 0;
+    //    string bffr = usrInp.substr(separator);
+    //
+    //    while (bffr.find(" ") != -1) {
+    //      int findPos = bffr.find(" ");
+    //      string cmdBffr = bffr.substr(0, findPos);
+    //
+    //      this->usrArgs.argsNodeInsert(cmdBffr);
+    //      this->totalArgs++;
+    //
+    //      bffr = bffr.substr(findPos + 1);
+    //    }
+    //
+    //    this->usrArgs.argsNodeInsert(bffr);
+    //    this->totalArgs++;
+
+    CharQueue bffr;
+    string cmd = "";
+    bool escapeCharFlag = false;
+    bool dQuotesFlag = false;
+
+    for (char c : this->usrInp) {
+      if (!escapeCharFlag) {
+        if (c == ' ' && cmd != "" && !dQuotesFlag) {
+          bffr.enq(cmd);
+          cmd = "";
+          continue;
+        } else if (c == '\\') {
+          escapeCharFlag = true;
+        } else if (c == '\"') {
+          dQuotesFlag = !dQuotesFlag;
+        }
+      } else {
+        escapeCharFlag = false;
+      }
+
+      cmd += c;
+    }
+
+    if (cmd != "") {
+      bffr.enq(cmd);
+    }
+
+    while (!bffr.isEmpty()) {
+      this->usrArgs.argsNodeInsert(bffr.deq());
+    }
+
+    this->usrArgs.argsNodeDisplay();
+
+    return NULL;
+  }
+
+public:
+  UsrCmd(string usrInp) {
+    this->usrInp = usrInp;
+    this->totalArgs = 0;
+    usrInpParser();
+    usrArgs.argsNodeDisplay();
+  }
 };
 
 void autoHead() {
@@ -11,8 +189,21 @@ void autoHead() {
 }
 
 int main(int argc, char *argv[]) {
-  if (argc == 1) {
-    autoHead();
+  string usrInp = "";
+
+  if (argc > 1) {
+    for (int i = 0; i < argc; i++) {
+      cout << argv[i] << endl;
+    }
+  }
+  autoHead();
+
+  while (usrInp != "exit") {
+    cout << ": ";
+    getline(cin, usrInp);
+    cout << endl;
+
+    UsrCmd cmd(usrInp);
   }
 
   return 0;
